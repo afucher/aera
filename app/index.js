6f6adef4a331e412165be7efa6c6e141d69a388f
Arthur Fucher <arthurfucher@gmail.com>
2016-12-19 16:45:46 -0200

    removed unused imports

--- app/index.js
+++ app/index.js
@@ -1,10 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import VueResource from 'vue-resource'
 
 import App from './components/App.vue'
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const router = new VueRouter({});
 
@@ -13,19 +12,4 @@ new Vue({
   router,
   render : (h) => h(App)
 
-})
-
-/*
-Vue.use(VueResource)
-
-
-export var router = new VueRouter()*/
-
-// Redirect to the home route if any routes are unmatched
-/*router.redirect({
-  '*': '/home'
-})*/
-
-
-// Start the app on the #app div
-//router.start(App, '#app')
+});
