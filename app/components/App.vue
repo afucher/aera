ff78fb12f5158b36bb483ee3d3af705fa86a1e60
Arthur Fücher <arthurfucher@gmail.com>
2016-12-19 16:37:57 -0200

    started front-end with Vue.js

---
+++ app/components/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+    <h1>Aera</h1>
+    {{ message }}
+    </div>
+    
+</template>
+<script>
+    export default{
+        data : () => {
+            return {message: 'Seja bem-vindo'}
+        }
+    }   
+</script>
