55ed02f3ea72dbc2f7f2212506f938901d24603b
Arthur Fücher <arthurfucher@gmail.com>
2017-03-20 23:19:36 -0300

    added vee-validator

--- app/components/client/NewClient.vue
+++ app/components/client/NewClient.vue
@@ -2,7 +2,8 @@
     <form @submit.prevent="createClient">
     <div class="form-group">
         <label>Nome</label>
-        <input type="text" v-model="client.name" class="form-control">
+        <input v-validate="'required'" name="name" type="text" v-model="client.name" class="form-control">
+        <span v-show="errors.has('name')" class="bg-danger">{{ errors.first('name') }}</span>
     </div>
     <div class="form-group">
         <label>CPF</label>
@@ -10,7 +11,8 @@
     </div>
     <div class="form-group">
         <label>E-mail</label>
-        <input type="text" v-model="client.email" class="form-control">
+        <input name="email" v-validate="'email'" type="text" v-model="client.email" class="form-control">
+        <span v-show="errors.has('email')" class="bg-danger">{{ errors.first('email') }}</span>
     </div>
     <div class="form-group">
         <label>Endereço</label>
@@ -42,6 +44,8 @@ export default {
     methods: {
         createClient(e) {
             const that = this;
+            this.$validator.validateAll();
+            if(this.errors.any()) return;
             that.$store.dispatch('createClient', this.client)
                 .then(() => that.$router.push('/clients'))
                 .catch((err) => this.errorMessage = err.body.message);
--- app/config.js
+++ app/config.js
@@ -5,10 +5,12 @@ import Vuex from 'vuex'
 
 import MyErrMsg from './components/util/ErrorMessage.vue'
 import {ServerTable, ClientTable, Event} from 'vue-tables-2';
+import VeeValidate from 'vee-validate';
 
 Vue.use(VueResource);
 Vue.use(VueRouter);
 Vue.use(Vuex);
+Vue.use(VeeValidate);
 
 Vue.component('MyErrMsg',MyErrMsg);
 Vue.use(ClientTable, {}, false);
