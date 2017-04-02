2b1242a8ab4d635e3f4acd20d50686b8da196c06
Arthur Fücher <arthurfucher@gmail.com>
2017-04-02 15:38:29 -0300

    added date field

--- src/utils/GroupsListPDF.js
+++ src/utils/GroupsListPDF.js
@@ -4,14 +4,19 @@ module.exports = (group) => {
     return new Promise((resolve, reject) => {
         let doc = new PDFDocument();
         let buffers = [];
-        doc.text(`Lista do Curso: ${group.Course.name}`);
+        doc.text(`Lista do Curso: ${group.Course.name}`,{continued:true});
+        doc.text(' '.repeat(20),{continued:true});
+        doc.text('Data: __/__/____')
         doc.moveDown();
         doc.text(`Professor: ${group.Teacher.name}`);
         doc.moveDown();
         doc.text(`Data de início: ${group.start_date}`);
+        doc.text('-'.repeat(117));
+        doc.moveDown();
+        doc.text('Alunos: ')
         group.Students.forEach(student => {
             doc.moveDown();
-            doc.text(student.name);
+            doc.text(`${student.name}: ${'_'.repeat(20)}`);
         })
         doc.on('data', buffers.push.bind(buffers) )
         doc.on('end', ()=>{
