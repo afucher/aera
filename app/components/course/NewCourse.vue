e4fbea9af04fbb05326ceefecb5f3ba9cd5a4c83
afucher <arthurfucher@gmail.com>
2017-08-13 21:45:42 -0300

    redirect to courses after create one

--- app/components/course/NewCourse.vue
+++ app/components/course/NewCourse.vue
@@ -1,21 +1,33 @@
 <template>
 <div>
     <CourseForm :course="course" v-on:formSubmit="createCourse"></CourseForm>
+    <MyErrMsg :errorMessage="errorMessage"></MyErrMsg>
 </div>
 </template>
 
 <script>
 import CourseForm from './CourseForm.vue'
+import MyErrMsg from '../util/ErrorMessage.vue'
 export default {
-    components:{CourseForm},
+    components:{CourseForm, MyErrMsg},
     data: function(){
         return {
-            course : {}
+            course : {},
+            errorMessage: ""
         }
     },
     methods: {
         createCourse(course) {
-            this.$store.dispatch('createCourse', course);
+            this.$validator.validateAll();
+            if(this.errors.any()) return;
+            const that = this;
+            this.$store.dispatch('createCourse', this.course)
+                .then((c)=>{
+                    that.$router.push({name:'courses'})
+                })
+                .catch((err) => {
+                    this.errorMessage = err;
+                });
         }
     }
 }
