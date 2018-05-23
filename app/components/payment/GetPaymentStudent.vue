fa5223de5cd00cde2e14c51e55e03ea74962238a
afucher <arthurfucher@gmail.com>
2018-05-22 21:44:42 -0300

    added month select to generate receipt

--- app/components/payment/GetPaymentStudent.vue
+++ app/components/payment/GetPaymentStudent.vue
@@ -1,7 +1,23 @@
 <template>
     <div>
         <modal name="select-month">
-            <input type="text" v-model="month"/>
+            <div class="form-group">
+                <label>Selecione o mês a ser gerado:</label>
+                <select v-model="month">
+                    <option value="1">Janeiro</option>
+                    <option value="2">Fevereiro</option>
+                    <option value="3">Março</option>
+                    <option value="4">Abril</option>
+                    <option value="5">Maio</option>
+                    <option value="6">Junho</option>
+                    <option value="7">Julho</option>
+                    <option value="8">Agosto</option>
+                    <option value="9">Setembro</option>
+                    <option value="10">Outubro</option>
+                    <option value="11">Novembro</option>
+                    <option value="12">Dezembro</option>
+                </select>
+            </div>
             <button @click.prevent="download">Download</button>
         </modal>
         <button @click.prevent="selectMonth">Gerar recibo</button>
