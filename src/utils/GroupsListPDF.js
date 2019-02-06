0a0832d5553fc02792373bb9a98cde8511fb106a
afucher <arthurfucher@gmail.com>
2019-02-06 21:33:38 -0200

    alterado layout da lista de presenca

--- src/utils/GroupsListPDF.js
+++ src/utils/GroupsListPDF.js
@@ -2,8 +2,9 @@
 const PDFDocument = require('pdfkit');
 module.exports = (group) => {
     return new Promise((resolve, reject) => {
-        let doc = new PDFDocument();
+        let doc = new PDFDocument({size:'A4', margin:50});
         let buffers = [];
+        doc.font('Courier');
         doc.text(`Lista do Curso: ${group.Course.name}`,{continued:true});
         doc.text(' '.repeat(20),{continued:true});
         doc.text('Data: __/__/____')
@@ -11,12 +12,12 @@ module.exports = (group) => {
         doc.text(`Professor: ${group.Teacher.name}`);
         doc.moveDown();
         doc.text(`Data de início: ${group.start_date}`);
-        doc.text('-'.repeat(117));
+        doc.text('-'.repeat(65));
         doc.moveDown();
         doc.text('Alunos: ')
         group.Students.forEach(student => {
             doc.moveDown();
-            doc.text(`${student.name}: ${'_'.repeat(20)}`);
+            doc.text(`${student.name}: ${'_'.repeat(60-student.name.length+2)}`);
         })
         doc.on('data', buffers.push.bind(buffers) )
         doc.on('end', ()=>{
